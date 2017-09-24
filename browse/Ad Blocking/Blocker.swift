fb99943adc753b593367e57cefa297943cffb5f4
Evan Brooks <brooks.ev@gmail.com>
2017-09-23 22:14:45 -0700

    Try smaller block list to avoid slowdowns

--- browse/Ad Blocking/Blocker.swift
+++ browse/Ad Blocking/Blocker.swift
@@ -10,7 +10,7 @@
 import UIKit
 import WebKit
 
-let EASYLIST_ID = "easylistrules"
+let blockerListId = "blockerListId"
 
 class Blocker: NSObject {
     static let shared = Blocker()
@@ -36,9 +36,9 @@ class Blocker: NSObject {
     
     func getList(_ done: @escaping () -> ()) {
         WKContentRuleListStore.default().getAvailableContentRuleListIdentifiers { (identifiers) in
-            if identifiers != nil && identifiers!.contains(EASYLIST_ID) {
+            if identifiers != nil && identifiers!.contains(blockerListId) {
                 print("existing rules found")
-                WKContentRuleListStore.default().lookUpContentRuleList(forIdentifier: EASYLIST_ID, completionHandler: { (list, error) in
+                WKContentRuleListStore.default().lookUpContentRuleList(forIdentifier: blockerListId, completionHandler: { (list, error) in
                     if let list : WKContentRuleList = list {
                         print("existing rules fetched")
                         self.ruleList = list
@@ -52,9 +52,9 @@ class Blocker: NSObject {
             } else {
                 print("compiling rules...")
                 do {
-                    if let path = Bundle.main.path(forResource: "easylist", ofType: "json") {
+                    if let path = Bundle.main.path(forResource: "ultimateAdblockList", ofType: "json") {
                         let data = try String(contentsOfFile:path, encoding: String.Encoding.utf8)
-                        WKContentRuleListStore.default().compileContentRuleList(forIdentifier: EASYLIST_ID, encodedContentRuleList: data) { (list, error) in
+                        WKContentRuleListStore.default().compileContentRuleList(forIdentifier: blockerListId, encodedContentRuleList: data) { (list, error) in
                             if let list : WKContentRuleList = list {
                                 print("rules compiled!")
                                 self.ruleList = list
