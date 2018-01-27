7af489d9707efb517df4abcca82aa99df69e2764
Evan Brooks <brooks.ev@gmail.com>
2018-01-26 16:39:25 -0800

    Move gradientview to separate file

--- browse/Tab Switcher/TabThumbnail.swift
+++ browse/Tab Switcher/TabThumbnail.swift
@@ -10,38 +10,6 @@ import UIKit
 
 typealias CloseTabCallback = (UICollectionViewCell) -> Void
 
-
-class GradientView : UIView {
-    
-    private let gradientLayer = CAGradientLayer()
-    
-    override var frame: CGRect {
-        didSet { resizeGradient() }
-    }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        resizeGradient()
-    }
-    
-    func resizeGradient() {
-        gradientLayer.frame = bounds
-        gradientLayer.frame.size.height = THUMB_H * 1.5
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        gradientLayer.colors = [ UIColor.black.withAlphaComponent(0).cgColor, UIColor.black.cgColor ]
-        gradientLayer.locations = [ 0.0, 1.0 ]
-        resizeGradient()
-        layer.addSublayer(gradientLayer)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
 class TabThumbnail: UICollectionViewCell, UIGestureRecognizerDelegate {
     
     var label : UILabel!
