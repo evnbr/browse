ad263e1a106433209e63b6248ddf20a4f928f6dc
Evan Brooks <brooks.ev@gmail.com>
2018-02-20 22:38:10 -0800

    use google typeahead

--- browse/Search Field/TypeaheadController.swift
+++ browse/Search Field/TypeaheadController.swift
@@ -9,33 +9,55 @@
 import Foundation
 
 // ddg search results:
-
-
 class Typeahead: NSObject {
     static let shared = Typeahead()
     
+    
+    func urlForQuery(_ query: String) -> URL? {
+//        return URL(string: "https://duckduckgo.com/ac/?q=\(query)")
+        return URL(string: "https://suggestqueries.google.com/complete/search?client=firefox&q=\(query)")
+    }
+    
+    func parseStrings(from suggestions: NSArray, maxCount: Int = .max) -> [String] {
+        return parseGoogle(from: suggestions, maxCount: maxCount)
+    }
+    
+    func parseDuckDuckGo(from data: NSArray, maxCount: Int) -> [String] {
+        var phrases : [String] = []
+        for item in data {
+            if let dict = item as? NSDictionary,
+                phrases.count < maxCount,
+                let phrase = dict.value(forKey: "phrase") as? String {
+                phrases.append(phrase)
+            }
+        }
+        return phrases
+    }
+
+    func parseGoogle(from data: NSArray, maxCount: Int) -> [String] {
+        var phrases : [String] = []
+        
+        guard let suggestions = data[1] as? NSArray else { return phrases }
+        for item in suggestions {
+            if let phrase = item as? String, phrases.count < maxCount {
+                phrases.append(phrase)
+            }
+        }
+        return phrases
+    }
+
+    
     func suggestions(for text: String, maxCount: Int = .max, completion: @escaping ([String]) -> Void) {
         let query = text.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
-        guard let url = URL(string: "https://duckduckgo.com/ac/?q=\(query)") else { return }
+        guard let url = urlForQuery(query) else { return }
         
         //fetching the data from the url
         URLSession.shared.dataTask(with: url, completionHandler: {(data, response, error) -> Void in
-            
             if let suggestions = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! NSArray {
-                
-                var phrases : [String] = []
-                for item in suggestions {
-                    if let dict = item as? NSDictionary, phrases.count < maxCount {
-                        if let phrase = dict.value(forKey: "phrase") as? String {
-                            phrases.append(phrase)
-                        }
-                    }
-                }
-                
-                DispatchQueue.main.async {
-                    completion(phrases)
-                }
+                let phrases = self.parseStrings(from: suggestions, maxCount: maxCount)
+                DispatchQueue.main.async { completion(phrases) }
             }
         }).resume()
     }
+    
 }
