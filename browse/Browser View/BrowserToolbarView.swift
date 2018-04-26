ffc1bc1194897b4449f5f10418fb406878a68175
Evan Brooks <brooks.ev@gmail.com>
2018-04-26 00:26:04 -0700

    cleanup

--- browse/Browser View/BrowserToolbarView.swift
+++ browse/Browser View/BrowserToolbarView.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class BrowserToolbarView: ColorToolbarView {
     
-    private let progressView: UIProgressView = UIProgressView(progressViewStyle: .default)
     var heightConstraint: NSLayoutConstraint!
     var backButton: ToolbarIconButton!
     var stopButton: ToolbarIconButton!
@@ -65,37 +64,8 @@ class BrowserToolbarView: ColorToolbarView {
     }
 
     
-    var progressOLD : Float {
-        get {
-            return progressView.progress
-        }
-        set {
-            progressView.alpha = 1.0
-            let isIncreasing = progressView.progress < newValue
-            
-            progressView.setProgress(0.1 + newValue * 0.8, animated: isIncreasing)
-            if (newValue >= 1.0) {
-                UIView.animate(withDuration: 0.3, delay: 0.3, options: .curveEaseOut, animations: {
-                    self.progressView.progress = 1.0
-                    self.progressView.alpha = 0
-                }, completion: { (finished) in
-                    self.progressView.setProgress(0.0, animated: false)
-                })
-            }
-        }
-    }
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        progressView.frame = CGRect(
-            origin: CGPoint(x: 0, y: frame.height - 2),
-            size:CGSize(width: UIScreen.main.bounds.size.width, height: 2)
-        )
-        progressView.trackTintColor = UIColor.clear
-        progressView.progressTintColor = UIColor.darkText
-        progressView.autoresizingMask = [.flexibleTopMargin, .flexibleWidth]
-//        addSubview(progressView)
         
         let blur = PlainBlurView(frame: bounds)
         blur.autoresizingMask = [.flexibleWidth, .flexibleHeight]
@@ -123,7 +93,6 @@ class BrowserToolbarView: ColorToolbarView {
         subviews.forEach { (v) in
             v.tintColor = tintColor
         }
-        progressView.progressTintColor = tintColor
     }
     
     
