50c78756e7ea8915d512b8f04ae5427232af8e54
Evan Brooks <brooks.ev@gmail.com>
2019-09-08 11:33:54 -0700

    pull scripts into own file

--- browse/Browser View/WebViewManager.swift
+++ browse/Browser View/WebViewManager.swift
@@ -42,21 +42,21 @@ class WebViewManager: NSObject {
         configuration.preferences.javaScriptCanOpenWindowsAutomatically = false
         configuration.userContentController.addUserScript(
             WKUserScript(
-                source: checkFixedFunc,
+                source: Scripts.checkFixed,
                 injectionTime: .atDocumentStart,
                 forMainFrameOnly: false
             )
         )
         configuration.userContentController.addUserScript(
             WKUserScript(
-                source: findLinkAtPointScript,
+                source: Scripts.findLinkAtPoint,
                 injectionTime: .atDocumentStart,
                 forMainFrameOnly: false
             )
         )
         configuration.userContentController.addUserScript(
             WKUserScript(
-                source: customStyleScript,
+                source: Scripts.customStyle,
                 injectionTime: .atDocumentEnd,
                 forMainFrameOnly: false
             )
@@ -117,143 +117,6 @@ extension WebViewManager: WKScriptMessageHandler {
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         if message.name == browseLoadHandlerName {
             loadingHandler?(message)
-        }
-    }
-}
-
-struct FixedNavResult {
-    let hasTopNav: Bool
-    let hasBottomNav: Bool
-}
-extension WKWebView {
-    func evaluateFixedNav(_ completionHandler: @escaping (FixedNavResult) -> Void) {
-        evaluateJavaScript("window.\(checkFixedFuncName)()") { (result, _) in
-            if let dict = result as? [String: Bool],
-                let top = dict["top"],
-                let bottom = dict["bottom"] {
-                completionHandler(FixedNavResult(
-                    hasTopNav: top,
-                    hasBottomNav: bottom))
-            }
-        }
-    }
-}
-
-private let checkFixedFuncName = "__BROWSE_HAS_FIXED_NAV__"
-
-private let checkFixedFunc = """
-    (function() {
-        const isFixed = (elm) => {
-            let el = elm;
-            while (typeof el === 'object' && el.nodeName.toLowerCase() !== 'body') {
-                const pos = window.getComputedStyle(el).getPropertyValue('position').toLowerCase()
-                if (pos === 'fixed' || pos === 'sticky' || pos === '-webkit-sticky') return true;
-                el = el.parentElement;
-            }
-            return false;
-        };
-        window.\(checkFixedFuncName) = () => {
-            return {
-                top: isFixed(document.elementFromPoint(1,1)),
-                bottom: isFixed(document.elementFromPoint(1,window.innerHeight - 1))
-            }
-        };
-    })();
-"""
-
-private let findLinkAtPointFuncName = "__BROWSE_GET_LINK__"
-private let clearHighlightedLinksFuncName = "__BROWSE__CLEAR_ACTIVE_LINK__"
-private let highlightLinkClassName = "__BROWSE_ACTIVE_LINK__"
-private let preventSelectionClassName = "__BROWSE_PREVENT_SELECTION__"
-
-private let findLinkAtPointScript = """
-    (function() {
-        window.\(findLinkAtPointFuncName) = (x, y) => {
-            let el = document.elementFromPoint(x, y);
-            if (!el) {
-                return "No el";
-            }
-            // Walk up to find parent with href
-            while (
-                typeof el === 'object'
-                && el.nodeName.toLowerCase() !== 'body'
-                && !el.hasAttribute('href')
-            ) {
-                el = el.parentElement;
-            }
-
-            const href = el.getAttribute('href');
-            if (!href) {
-                return "No href for " + el.tagName;
-            }
-            document.body.classList.add('\(preventSelectionClassName)');
-            el.classList.add('\(highlightLinkClassName)');
-            return {
-                href: href,
-                title: el.getAttribute('title')
-            };
-        };
-        window.\(clearHighlightedLinksFuncName) = () => {
-            const els = document.querySelectorAll('.\(highlightLinkClassName)');
-            document.body.classList.remove('\(preventSelectionClassName)');
-            for (const el of els) {
-                el.classList.remove('\(highlightLinkClassName)');
-            }
-        };
-    })();
-"""
-
-private let customStyleScript = """
-    const style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerText = `
-        .__BROWSE_PREVENT_SELECTION__ *:not(input):not(textarea) {
-            -webkit-user-select: none;
-            -webkit-touch-callout: none;
-        }
-        .\(highlightLinkClassName) {
-            transition: all 0.3s;
-            background: rgba(0,0,0,0.1);
-            transform: scale(1.08);
-        }
-    `;
-    const head = document.getElementsByTagName('head')[0];
-    head.appendChild(style);
-"""
-
-struct LinkInfo {
-    let href: String
-    let title: String?
-}
-
-extension WKWebView {
-    func linkAt(_ position: CGPoint, completionHandler: @escaping ((LinkInfo?) -> ()) ) {
-        
-        // Translate gesture point into the coordinate system of the zoomed page
-        let scaleFactor = 1 / scrollView.zoomScale
-        let pt = CGPoint(x: position.x * scaleFactor, y: position.y * scaleFactor)
-        
-        self.evaluateJavaScript("window.\(findLinkAtPointFuncName)(\(pt.x), \(pt.y))") { (val, err) in
-            if let err = err {
-                print(err)
-            }
-            if let dict = val as? [String: String?],
-                let href = dict["href"], href != nil,
-                let title = dict["title"] {
-                completionHandler(LinkInfo(href: href!, title: title))
-                return
-            }
-            
-            print("no link info: \(val ?? "Missing val")")
-            completionHandler(nil)
-        }
-    }
-    
-    func clearHighlightedLinks() {
-        self.evaluateJavaScript("window.\(clearHighlightedLinksFuncName)()") { (val, err) in
-            if let err = err {
-                print(err)
-            }
         }
     }
 }
